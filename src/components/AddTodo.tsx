37ba3a7ea7410b8ed8a2da647a8d2dcef2d74ce0
mpoweredo <mpow3r1337@gmail.com>
2022-10-19 17:35:14 +0200

    Added AddTodo component :sparkles:

---
+++ src/components/AddTodo.tsx
@@ -0,0 +1,12 @@
+import { Flex, Input, Button } from '@chakra-ui/react';
+
+const AddTodo = () => {
+	return (
+		<Flex gap='2' direction={{ base: 'column', sm: 'row' }}>
+			<Input placeholder='Make dinner...' _focus={{ bg: 'gray.900' }} _hover={{ bg: 'gray.600' }} variant='filled' maxWidth='8xl' bg='gray.700' />
+			<Button bg='gray.500'>Add todo</Button>
+		</Flex>
+	);
+};
+
+export default AddTodo;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
-import { Text } from '@chakra-ui/react';
+import { Box, Center, Container, Text } from '@chakra-ui/react';
 import type { NextPage } from 'next';
+import AddTodo from '../components/AddTodo';
 
 const Home: NextPage = () => {
 	return (
-		<div>
-			<Text>Hello world</Text>
-		</div>
+		<Container height='100vh'>
+			<Box>
+				<Center height='80px'>
+					<Text textAlign='center' fontSize='3xl' fontWeight='semibold' color='gray.300'>
+						Your last todo list!
+					</Text>
+				</Center>
+				<AddTodo />
+			</Box>
+		</Container>
 	);
 };
 
